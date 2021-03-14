4a1d2c91b095de94d2376e1bf9471c6ef7004e81
wangxiang <xiang_wang@intsig.net>
2021-03-14 20:58:52 +0800

    disk

---
+++ xmetrics/disk/disk_darwin.go
@@ -0,0 +1,25 @@
+// +build linux
+
+package disk
+
+func Get() ([]Stats, error) {
+	return []Status{}, nil
+}
+
+// Stats represents disk I/O statistics for linux.
+type Stats struct {
+	MajorNumber      uint64
+	MinorNumber      uint64
+	DeviceName       string
+	ReadsCompleted   uint64
+	ReadsMerged      uint64
+	SectorsRead      uint64
+	TimeSpentReading uint64
+	WritesCompleted  uint64
+	WritesMerged     uint64
+	SectorsWritten   uint64
+	TimeSpentWriting uint64
+	IOInProgress     uint64
+	TimeSpentInIO    uint64
+	WeightedTimeInIO uint64
+}
