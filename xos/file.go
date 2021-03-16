3e0d32a7db257869193caef6ee167b336bf2526d
wangxiang <xiang_wang@intsig.net>
2021-03-16 16:27:06 +0800

    xos

--- xos/file.go
+++ xos/file.go
@@ -12,6 +12,7 @@ const (
 	MODE_FILE = 1
 )
 
+// 返回下层所有文件或目录
 func ListSubFiles(path string, mode int) ([]string, error) {
 	var r []string
 	d, err := ioutil.ReadDir(path)
@@ -33,6 +34,7 @@ func ListSubFiles(path string, mode int) ([]string, error) {
 	return r, nil
 }
 
+// 返回符合特定后缀的文件 或者目录
 func ListSubFilesRecur(path string, suffix string, mode int) (files []string, err error) {
 	files = make([]string, 0, 30)
 	err = filepath.Walk(path, func(filename string, fi os.FileInfo, err error) error {
@@ -41,6 +43,28 @@ func ListSubFilesRecur(path string, suffix string, mode int) (files []string, er
 		}
 		if mode == MODE_FILE {
 			if !fi.IsDir() && strings.HasSuffix(fi.Name(), suffix) {
+				files = append(files, filename)
+			}
+			return nil
+		} else if mode == MODE_DIR {
+			if fi.IsDir() {
+				files = append(files, filename)
+			}
+		}
+		return nil
+	})
+	return files, err
+}
+
+// 返回所有文件或目录 递归
+func ListAllFilesRecur(path string, mode int) (files []string, err error) {
+	files = make([]string, 0, 30)
+	err = filepath.Walk(path, func(filename string, fi os.FileInfo, err error) error {
+		if err != nil {
+			return nil
+		}
+		if mode == MODE_FILE {
+			if !fi.IsDir() {
 				files = append(files, filename)
 			}
 			return nil
