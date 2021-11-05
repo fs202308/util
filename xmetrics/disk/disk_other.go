d5c12855fc38fd810d00a2059e4a951ec3fc8926
bsync-tech <76582032+bsync-tech@users.noreply.github.com>
2021-11-05 11:01:39 +0800

    Create disk_other.go

--- xmetrics/disk/disk_other.go
+++ xmetrics/disk/disk_other.go
@@ -3,18 +3,6 @@
 
 package disk
 
-import (
-	"bufio"
-	"bytes"
-	"fmt"
-	"io"
-	"os"
-	"os/exec"
-	"strconv"
-	"strings"
-	"time"
-)
-
 type disks map[string]map[string]string
 
 func Iostat() disks {
